da18a91665474efd2610bf37727f9e95b1fffccd
soorq <fantastfad830@gmail.com>
2026-05-12 00:15:39 +0300

    chore(db): upgrade drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,15 @@ export default defineConfig({
     schema: './src/shared/entities/index.ts',
     out: './migrations',
     dialect: 'postgresql',
+    breakpoints: false,
+    casing: 'snake_case',
+    migrations: {
+        prefix: 'index',
+        table: 'migrations',
+        schema: 'drizzle',
+    },
+    strict: true,
+    verbose: true,
     dbCredentials: {
         url: process.env.DATABASE_URL!,
     },
